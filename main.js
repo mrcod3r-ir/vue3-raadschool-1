f19c7e547d5bc4f9497f21abd71c97d97faa2876
raadschooljs <raadschooljs@gmail.com>
2022-04-12 18:58:51 +0430

    add remove modal button by methods

--- main.js
+++ main.js
@@ -5,28 +5,35 @@ Vue.createApp({
         taskTitle:'',
         taskDesc:'',
         showDesc:true,
+        displayRemoveModal:false,
+        removeItemIndex:null,
         doList:[
           {
+           
             title:"Lorem, ipsum.",
             desc:'some description 1',
             completed:false,
           },
           {
+           
             title:"Lorem, ipsum dolor.",
             desc:'some description 3',
             completed:false,
           },
           {
+           
             title:"asjdfasdf ",
             desc:'some description 4',
             completed:true,
           },
           {
+           
             title:"as4df6 Lorem, ipsum.",
             desc:'some description f',
             completed:false,
           },
-        ]
+        ],
+
       }
     },
     methods:{
@@ -46,8 +53,18 @@ Vue.createApp({
           this.taskDesc = ''
         }
       },
-      removeItem(){
-        console.log('remove item');
+      showRemoveModal(i){
+
+        // show modal
+        this.displayRemoveModal = true;
+        this.removeItemIndex = i
+      },
+      removeItem(index){
+        // show modal
+        // this.showRemoveModal = true;
+        this.doList.splice(index, 1);
+        this.displayRemoveModal = false;
+        this.removeItemIndex = null
       }
     }
   }).mount('#app')
